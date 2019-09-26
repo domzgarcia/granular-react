226ec5dfa0b0b7b7b092e9d4245e3942e7fad2a2
Domz Garcia <dom.garcia@nuworks.ph>
2019-09-26 20:53:06 +0800

    updates

--- .storybook/stories/button.stories.js
+++ .storybook/stories/button.stories.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import Button from 'Components/button/Button';
 
-import 'Common/global.scss';
 import 'Styles/buttons.scss';
+import 'Common/global.scss';
 
 storiesOf('Button', module)
     .add('No text', () => (
-        <Button block className="red">Boom</Button>
+        <Button className="red">Boom</Button>
     ))
