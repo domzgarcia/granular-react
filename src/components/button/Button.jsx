58b805b968d0d81c60eb34b5a51aa74c4387fa9c
Domz Garcia <dom.garcia@nuworks.ph>
2019-09-27 15:53:45 +0800

    test

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,7 +1,6 @@
 import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
 import classnames from 'classnames'
-
 import blacklist from 'blacklist'
 
 class Button extends PureComponent {
